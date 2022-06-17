fa8f831356f5e14cf9079e767094565a0b0a9531
SanteIsHere <r.a.frye00@gmail.com>
2022-06-17 15:05:08 -0400

    Implement password validation (JavaScript)

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const passFields = document.querySelectorAll(".pword")
+let passFields = document.querySelectorAll(".pword")
+passFields.forEach((f) => {f.setCustomValidity("Passwords must match!")})
+let passFieldsArray = Array.from(passFields)
 const registerButt = document.getElementById("login-butt")
 
 const validate = function() {
-    if (passFields[0].textContent != passFields[1].textContent) {
-        console.log("Passwords do not match!")
+    if (!(passFieldsArray.every((field) => {if (field.value === passFieldsArray[0].value) {return true}}))) {
+        console.log("Passwords are not equal!")
+        passFields.forEach((f) => {f.reportValidity()})
+    } else {
+        console.log("Passwords equal")
     }
 }
+
+registerButt.addEventListener("click", validate)
 
