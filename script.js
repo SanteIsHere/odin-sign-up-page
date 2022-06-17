157b766e8f5861c65c7dcc374a77a73d97847f8e
SanteIsHere <r.a.frye00@gmail.com>
2022-06-16 20:00:03 -0400

    Attemot to fix width of page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const passFields = document.querySelectorAll(".pword")
 const registerButt = document.getElementById("login-butt")
 
 const validate = function() {
-    if (passFields[0].innerHTML != passFields[1].innerHTML) {
+    if (passFields[0].textContent != passFields[1].textContent) {
         console.log("Passwords do not match!")
     }
 }
