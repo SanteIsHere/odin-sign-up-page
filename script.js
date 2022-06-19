a903068e9fd986ce872dae44ed4eaf40b2f097f0
SanteIsHere <r.a.frye00@gmail.com>
2022-06-19 13:46:13 -0400

    Link button to form and inputs. TODO: Fix height (empty white space)

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let passFields = document.querySelectorAll(".pword")
-let inputs = document.querySelectorAll("input")
-passFields.forEach((f) => {f.setCustomValidity("Passwords must match!")})
-let passFieldsArray = Array.from(passFields)
-const registerButt = document.getElementById("login-butt")
+// let passFields = document.querySelectorAll(".pword")
+// let inputs = document.querySelectorAll("input")
+// passFields.forEach((f) => {f.setCustomValidity("Passwords must match!")})
+// let passFieldsArray = Array.from(passFields)
+// const registerButt = document.getElementById("login-butt")
 
-const validate = function() {
-    if (!(passFieldsArray.every((field) => {if (field.value === passFieldsArray[0].value) {return true}}))) {
-        console.log("Passwords are not equal!")
-        passFields.forEach((f) => {f.reportValidity()})
-    } else {
-        console.log("Passwords equal")
-    }
-    
-    if (Array.from(inputs).every((inp) => {if (inp.value === "") {return true}})) {
-        inputs.forEach((inp) => {inp.reportValidity()})
-    }
+// const validate = function() {
+//     if (!(passFieldsArray.every((field) => {if (field.value === passFieldsArray[0].value) {return true}}))) {
+//         console.log("Passwords are not equal!")
+//         passFields.forEach((f) => {f.reportValidity()})
+//     } else {
+//         console.log("Passwords equal")
+//     }
+// }
 
-}
-
-registerButt.addEventListener("click", validate)
+// registerButt.addEventListener("click", validate)
 
